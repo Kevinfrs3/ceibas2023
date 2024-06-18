1988956906d7a4208104f5ddb29bac3b09f01c54
dairo santana <dairosantab@gmail.com>
2024-06-18 09:37:34 -0500

    Se creo el menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,48 +4,40 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-
-
 export class MenuComponent implements OnInit {
-
   constructor(private renderer: Renderer2) {}
 
   executeTawkToScript() {
     var s1 = this.renderer.createElement('script');
     var s0 = document.getElementsByTagName('script')[0];
-  
+
     s1.type = 'text/javascript';
     s1.async = true;
     s1.src = 'https://embed.tawk.to/65a64d5f8d261e1b5f53db52/1hk8psfo3';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
-    
+
     this.renderer.appendChild(s0.parentNode, s1);
   }
 
   openTawkChat() {
     this.executeTawkToScript();
     // Mostrar el widget de chat
-  }  
-
+  }
 
   afAuth: any;
   router: any;
   openSidenav() {
-    console.log(document.querySelectorAll('body')[0])
-    document.querySelectorAll('body')[0].classList.add('mobile-menu-visible')
+    console.log(document.querySelectorAll('body')[0]);
+    document.querySelectorAll('body')[0].classList.add('mobile-menu-visible');
   }
   logOut() {
     this.afAuth.signOut().then(() => this.router.navigate(['/login']));
   }
 
-
-
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void {}
 
   showInfo = false;
 
@@ -53,4 +45,9 @@ export class MenuComponent implements OnInit {
     this.showInfo = !this.showInfo;
   }
 
+  isSectionVisible = false;
+
+  toggleSection() {
+    this.isSectionVisible = !this.isSectionVisible;
+  }
 }
